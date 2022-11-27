958a86633d43e7820860c58b42c8aa9b332b89a1
grangegermi <dasha.voloshina@gmail.com>
2022-11-27 16:26:12 +0300

    first

--- Api2/ViewController.swift
+++ Api2/ViewController.swift
@@ -4,16 +4,73 @@
 //
 //  Created by Даша Волошина on 26.11.22.
 //
+//Самостоятельная работа: самостоятельно загрузить данные с выбранного
+//АПИ
+//https://jsonplaceholder.typicode.com/photos, https://jsonplaceholder.typicode.com/users, распарсить, взять первых 10 элементов, Подменить id внутри Photos на самого Юзера, отобразить на экране ячейку в TableView. Показать Картинку(спользуем SDWebImage или Kingfisher) и Имя пользователя. При нажатии на ячейку -> Удалить из локальных данных Photo(Отправить запрос на сервер DELETE). В navigationBar добавляем “+”. По нажатии на него отправляем запрос на создание ресурса (POST). Незабываем обновлять данные.
+//PS. Для создания запросов Post и Delete есть гайд - https://jsonplaceholder.typicode.com/guide/ , тк этот сервис предоставляет тестовые данные, POST и DELETE на основные данные не повлияют. И как всегда если будут вопросы - я к ним готов. На это задание у вас будет время до 28 ноября. Почему такой срок, потому что это похоже на тестовое задание для джунов
+
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let urlPhoto = URL(string: "https://jsonplaceholder.typicode.com/photos")!
+    let urlUser = URL(string: "https://jsonplaceholder.typicode.com/users")!
+//    var newArrayPhotos: ArraySlice<Photos> = []
+//    var newArrayUser: ArraySlice<User> = []
+    var finalPhotos:FinalPhotos?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        var urlArray:[URL] = [urlPhoto, urlUser]
+        var group = DispatchGroup()
+        let queueConcurrent = DispatchQueue(label: "urls", attributes: .concurrent)
+        
+        for url in urlArray {
+                
+            group.enter()
+            queueConcurrent.async {
+                    
+            var request = URLRequest(url: url)
+            request.httpMethod = "GET"
+                let task = URLSession.shared.dataTask(with: request) { [ weak self] data, response, error in
+                        
+                guard let data = data else{return}
+
+                 do {
+                     let finalData = try JSONDecoder().decode([FinalPhotos].self , from:  data)
+//                     print(finalData.first?.albumId)
+
+                    var newArrayPhotos = finalData.prefix(through: 9)
+//                     print (newArrayPhotos.count)
+
+                 } catch {
+                    print(error)
+                        }
+  
+                do {
+                    let finalData2 = try JSONDecoder().decode([User].self , from:  data)
+
+                    var newArrayUser = finalData2.prefix(through: 9)
+//                     print (newArrayUser.count)
+
+                } catch {
+                   print(error)
+                       }
+                        
+                    }
+                    task.resume()
+
+                    group.leave()
+             
+                }
+                group.wait()
+            }
+         
+              group.notify(queue: .main){
+                  print("All uploaded")
+              }
+        
     }
-
-
 }
-
---
+++ Api2/json.swift
@@ -0,0 +1,97 @@
+//
+//  json.swift
+//  Api2
+//
+//  Created by Даша Волошина on 26.11.22.
+//
+
+import Foundation
+
+
+struct FinalPhotos:Decodable {
+    
+    let albumId:Int
+    let id: User
+    let title: String
+    let url: URL
+    let thumbnailUrl: URL
+    
+}
+
+//{
+//   "albumId": 1,
+//   "id": 1,
+//   "title": "accusamus beatae ad facilis cum similique qui sunt",
+//   "url": "https://via.placeholder.com/600/92c952",
+//   "thumbnailUrl": "https://via.placeholder.com/150/92c952"
+// }
+
+struct  Photos:Decodable {
+    
+    let albumId:Int
+    let id: Int
+    let title: String
+    let url: URL
+    let thumbnailUrl: URL
+    
+    
+}
+
+//
+//{
+//   "id": 1,
+//   "name": "Leanne Graham",
+//   "username": "Bret",
+//   "email": "Sincere@april.biz",
+//   "address": {
+//     "street": "Kulas Light",
+//     "suite": "Apt. 556",
+//     "city": "Gwenborough",
+//     "zipcode": "92998-3874",
+//     "geo": {
+//       "lat": "-37.3159",
+//       "lng": "81.1496"
+//     }
+//   },
+//   "phone": "1-770-736-8031 x56442",
+//   "website": "hildegard.org",
+//   "company": {
+//     "name": "Romaguera-Crona",
+//     "catchPhrase": "Multi-layered client-server neural-net",
+//     "bs": "harness real-time e-markets"
+//   }
+// },
+
+struct User:Decodable {
+    
+    let id:Int
+    let name:String
+    let username :String
+    let email:String
+    let address:Adress
+    let phone:String
+    let website:String
+    let company:Company
+    
+}
+
+struct Adress:Decodable{
+    
+    let street:String
+    let suite:String
+    let city:String
+    let zipcode:String
+    let geo:Geo
+}
+
+struct Geo:Decodable{
+    let lat: String
+    let lng:String
+}
+
+struct Company:Decodable{
+    let name:String
+    let catchPhrase:String
+    let bs:String
+    
+}
